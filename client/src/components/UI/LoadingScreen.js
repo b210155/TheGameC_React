d2b492f1c818b4bff56f5b7c26cfe40462a16cfa
LAPTOP-GHBD2O7D\黃子洋 <b210155@gmail.com>
2023-10-29 03:27:02 +0800

    新增頁面讀取畫面

---
+++ client/src/components/UI/LoadingScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import classes from "./LoadingScreen.module.css";
+
+const LoadingScreen = () => {
+  return (
+    <div className={classes.loadingScreen}>
+      <span>Loading</span>
+    </div>
+  );
+};
+
+export default LoadingScreen;
