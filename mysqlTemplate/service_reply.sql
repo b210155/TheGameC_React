11424329e6ccae92fffc21aed1eb9155e03444f7
LAPTOP-GHBD2O7D\黃子洋 <b210155@gmail.com>
2023-11-24 03:22:39 +0800

    更改客服信件主題資料結構

--- mysqlTemplate/service_reply.sql
+++ mysqlTemplate/service_reply.sql
@@ -1,9 +1,9 @@
 -- 客服資料表
 CREATE TABLE service_reply ( 
   service_id INT PRIMARY KEY AUTO_INCREMENT, 
-  user_id INT NOT NULL,  -- 使用回函的會員
+  user_id INT NOT NULL,  -- 會員
   category VARCHAR(10) NOT NULL, -- 回報類別
-  subject VARCHAR(25)  NOT NULL, -- 填入主旨
+  subject TEXT NOT NULL, -- 填入主旨
   description TEXT NOT NULL, -- 說明欄位
   is_resolved BOOLEAN NOT NULL DEFAULT 0, -- 問題是否解決? 0 / 1
   FOREIGN KEY (user_id) REFERENCES users(user_id)
